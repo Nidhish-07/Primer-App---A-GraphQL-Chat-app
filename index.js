b2c24102c61fc0cb57cef3f9d5ba10bc73ac3621
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-02-04 20:39:17 +0530

    fetching user data

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-import {  ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
-import {gql} from "apollo-server"
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { gql } from "apollo-server";
+
+const users = [
+    {
+        id: "1",
+        firstName: "John",
+        lastName: "Doe",
+        email: "john@gmail.com",
+    },
+    {
+        id: "2",
+        firstName: "Jane",
+        lastName: "Doe",
+        email: "jane@gmail.com",
+    }
+];
 
 const typeDefs = gql`
-type Query{
-    greet:String
-}
-`
+  type Query {
+    users:[User]
+    user(id:ID!):User
+  }
+
+  type User{
+    id:ID
+    firstName: String
+    lastName: String
+    email: String
+  }
+
+
+  
+`;
 
 const resolvers = {
     Query: {
-        greet: () => "Hello, world!"
-    }
-}
+        users: () => users,
+        user: (parent, args, context) => {
+            return users.find(user=>user.id===args.id)
+        }
+    },
+};
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers });
 
-
-
-
-
-
-const { url } = await startStandaloneServer(server, { listen: { port: 8080 } })
+const { url } = await startStandaloneServer(server, { listen: { port: 8080 } });
 
 console.log(`Server is listening on ${url}`);
