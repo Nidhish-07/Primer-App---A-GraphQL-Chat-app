84281d0d2851904128794cff8e41aada1628ff05
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-02-04 22:56:34 +0530

    Create userr added

--- index.js
+++ index.js
@@ -1,46 +1,61 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { gql } from "apollo-server";
+import { randomUUID } from "crypto";
 
 const users = [
-    {
-        id: "1",
-        firstName: "John",
-        lastName: "Doe",
-        email: "john@gmail.com",
-    },
-    {
-        id: "2",
-        firstName: "Jane",
-        lastName: "Doe",
-        email: "jane@gmail.com",
-    }
+  {
+    id: "1",
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@gmail.com",
+  },
+  {
+    id: "2",
+    firstName: "Jane",
+    lastName: "Doe",
+    email: "jane@gmail.com",
+  },
 ];
 
 const typeDefs = gql`
   type Query {
-    users:[User]
-    user(id:ID!):User
+    users: [User]
+    user(id: ID!): User
   }
 
-  type User{
-    id:ID
+  type User {
+    id: ID
     firstName: String
     lastName: String
     email: String
   }
 
+  input UserInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+  }
 
-  
+  type Mutation {
+    createUser(newUser: UserInput!): User
+  }
 `;
 
 const resolvers = {
-    Query: {
-        users: () => users,
-        user: (parent, args, context) => {
-            return users.find(user=>user.id===args.id)
-        }
+  Query: {
+    users: () => users,
+    user: (parent, args, context) => users.find((user) => user.id === args.id),
+  },
+  Mutation: {
+    createUser: (parent, args, context) => {
+      const userNew = { id: randomUUID(), ...args.newUser };
+      users.push(userNew);
+
+      return userNew;
     },
+  },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
